f97f7c22316546ce2ad9cade1941314db7923733
José Caldas <jrcaldas@yahoo.com.br>
2021-08-01 21:57:09 -0300

    Add Button Component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import * as S from "./styles";
+
+export type ButtonProps = {
+  children: ReactNode;
+  size: "small" | "medium" | "large";
+  fullWidth?: boolean;
+  icon?: JSX.Element;
+};
+
+const Button = ({
+  children,
+  size = "medium",
+  fullWidth = false,
+  icon,
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+    {icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+);
+
+export default Button;
